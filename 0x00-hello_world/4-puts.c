e51ceb7d8c25117ed48bd4a77c1628a1eb00ac8e
DavidPerezF <you@example.com>
2021-06-22 02:06:17 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/*Este es el main*/
 int main(void)
 {
-puts('"Programming is like building a multilingual puzzle');
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
